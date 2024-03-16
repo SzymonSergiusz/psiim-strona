94ab8082d70f7705913a6f16636dc9513e46eb46
SzymonSergiusz <62117922+SzymonSergiusz@users.noreply.github.com>
2024-03-16 22:00:47 +0100

    +achievements +achievement card

---
+++ components/AchievementCard.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const props = defineProps(['isUnlocked'])
+console.log(props.isUnlocked)
+let icon = ''
+let color = ''
+if (props.isUnlocked) {
+  color = 'text-green-600'
+  icon = 'i-heroicons-lock-open'
+} else {
+  color = 'text-gray-600'
+  icon = 'i-heroicons-lock-closed'
+
+}
+
+console.log(color)
+
+
+</script>
+<template>
+  <UCard class="w-3/4 m-1.5 p-1.5" :class="color">
+    <div class="flex justify-between">
+      <div class="flex items-center">
+        <div class="mr-1">
+          <UIcon class="size-6" :name="icon"/>
+        </div>
+        <div>
+          <h2 class="text-center">Nazwa osiągnięcia</h2>
+        </div>
+      </div>
+      <div class="flex items-center">
+        <div>
+          <p class="text-left">Opis za co jest osiągnięcie</p>
+        </div>
+      </div>
+      <div v-if="isUnlocked" class="flex items-center">
+        <div>
+          <p class="text-right">Data osiągnięcia</p>
+        </div>
+      </div>
+      <div v-else class="w-1/5 items-center">
+        <h2>Postęp: x/y</h2>
+        <UProgress class="" :value="2" :max="10"/>
+      </div>
+
+
+    </div>
+  </UCard>
+
+</template>
+
+<style scoped>
+</style>
--- pages/user/achievements.vue
+++ pages/user/achievements.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
+const links = [{
+  label: 'Odblokowane',
+  icon: "i-heroicons-lock-open"
+},
+  {
+    label: 'Do odblokowania',
+    icon: "i-heroicons-lock-closed"
+  }
 
+]
 </script>
-
+<!--https://ui.nuxt.com/components/tabs#slots-->
 <template>
-  <p>Lista osiagnięć</p>
+  <div class="p-2.5 m-2.5">
+    <h2 >Twoje postępy: </h2>
+    <UProgress :value="0" :max="10"/>
+  </div>
+  <div class="flex justify-center">
+
+    <div class="flex flex-col items-center w-5/6 ">
+      <AchievementCard :is-unlocked="true"/>
+      <AchievementCard :is-unlocked="true"/>
+      <AchievementCard :is-unlocked="true"/>
+      <AchievementCard :is-unlocked="true"/>
+      <AchievementCard :is-unlocked="false"/>
+      <AchievementCard :is-unlocked="false"/>
+      <AchievementCard :is-unlocked="false"/>
+    </div>
+  </div>
+
+
+
 </template>
 
 <style scoped>
