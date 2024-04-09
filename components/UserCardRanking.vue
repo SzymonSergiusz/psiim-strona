b8f9e64d6c8227e4ae8351cba82dde903149ff4f
Kacper Krzesinski <k.krzesinski02@gmail.com>
2024-04-09 20:03:23 +0200

    Added footer and ranking page

---
+++ components/UserCardRanking.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    const props = defineProps(['place'])
+</script>
+
+
+<template>
+  <UCard class="w-3/4 m-1.5 p-1.5" :class="color">
+    <div class="flex justify-between">
+        <div>
+          <h2 class="text-left">{{ place }}</h2>
+        </div>
+      <div class="flex items-center">
+        <div>
+          <h2 class="text-center">Nazwa użytkownika</h2>
+        </div>
+      </div>
+      <div class="flex items-center">
+        <div>
+          <p class="text-right">Liczba zdobytych osiągnięć</p>
+        </div>
+      </div>
+    </div>
+  </UCard>
+
+</template>
+
+<style scoped>
+</style>
