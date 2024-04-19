328624da951679de05c91e318f0b0a6f1e0a2636
piotrek121252 <piotrekkomarnicki01@gmail.com>
2024-04-19 17:46:26 +0200

    Added start page and favicon

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///C:/Users/kacpe/Documents/studia/psim/psiim-strona/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export { default } from "file:///C:/Users/piotr/Desktop/PSIM/PSIM_Strona/psiim-strona/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
