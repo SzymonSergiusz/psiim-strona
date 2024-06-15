b6c215be205dac748a18cd368b4ddce5b585a778
piotrek121252 <piotrekkomarnicki01@gmail.com>
2024-06-15 19:52:34 +0200

    Added ranking, achievements, collection.

--- pages/ranking.vue
+++ pages/ranking.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+// import { ref, onMounted } from 'vue';
+// import UserCardRanking from '~/components/UserCardRanking.vue';
+
 const links = [{
   label: 'Odblokowane',
   icon: "i-heroicons-lock-open"
@@ -9,22 +12,37 @@ const links = [{
   }
 
 ]
+
+const config = useRuntimeConfig();
+const serverUrl = config.public.serverUrl;
+
+const ranking = ref([]);
+
+async function getRanking() {
+  try {
+    const response = await fetch(`${serverUrl}/ranking`);
+    const data = await response.json();
+    // ranking.value = data.ranking;
+    ranking.value = data.ranking.sort((a, b) => b.count - a.count);
+    console.log(ranking.value)
+    console.log(data)
+  } catch (error) {
+    console.error('Error fetching ranking:', error);
+  }
+}
+
+onMounted(() => {
+  getRanking();
+});
+
+
 </script>
 <!--https://ui.nuxt.com/components/tabs#slots-->
 <template>
   <div class="flex justify-center">
 
     <div class="flex flex-col items-center w-5/6 ">
-      <UserCardRanking :place="1"/>
-      <UserCardRanking :place="2"/>
-      <UserCardRanking :place="3"/>
-      <UserCardRanking :place="4"/>
-      <UserCardRanking :place="5"/>
-      <UserCardRanking :place="6"/>
-      <UserCardRanking :place="7"/>
-      <UserCardRanking :place="8"/>
-      <UserCardRanking :place="9"/>
-      <UserCardRanking :place="10"/>
+      <UserCardRanking v-for="(r, index) in ranking" :key="r.username" :username="r.username" :count="r.count" :rank="index + 1" />
     </div>
   </div>
 
